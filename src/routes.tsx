d5e56a082529dcef580d5cc82270e6bdd8bcad1c
Gibson Cruz <gibson8fla@gmail.com>
2024-12-22 20:10:06 -0300

    feat: adds login to route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout } from './pages/Layout'
 import { Index } from './pages/Index'
+import { Login } from './components/Login/Login'
 
 export const AppRoute = () => {
     return (
         <Router>
             <Routes >
+                <Route path='/login' element={<Login />} />
                 <Route element={<Layout />}>
                     <Route path='/' element={<Index />} />
                 </Route>
