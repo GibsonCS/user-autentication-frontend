42439353124b8a4edf63c1e250c4a19298e85bde
Gibson Cruz <gibson8fla@gmail.com>
2025-01-14 10:51:21 -0300

    chore: gets login page in the /

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout } from './pages/Layout'
-import { Index } from './pages/Index'
+import { Dashboard } from './pages/Dashboard'
 import { Login } from './components/Login/Login'
+import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute'
 
 export const AppRoute = () => {
     return (
         <Router>
             <Routes >
-                <Route path='/login' element={<Login />} />
-                <Route element={<Layout />}>
-                    <Route path='/' element={<Index />} />
+                <Route path='/' element={<Login />} />
+                <Route element={<ProtectedRoute requiredRoles={["user"]} />}>
+                    <Route element={<Layout />}>
+                        <Route path='/dashboard' element={<Dashboard />} />
+                    </Route>
                 </Route>
             </Routes>
         </Router>
