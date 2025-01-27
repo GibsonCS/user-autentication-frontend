b869dd25c1430290630059a58778c562f89822db
Gibson Cruz <gibson8fla@gmail.com>
2025-01-27 12:10:55 -0300

    chore: implements loading

---
+++ src/Components/Loading/Loading.tsx
@@ -0,0 +1,8 @@
+
+export const Loading = () => {
+    return (
+        <div className="bg-sky-950 flex flex-col justify-center items-center h-svh">
+            <span className=" animate-pulse text-5xl font-semibold">Carregando...</span>
+        </div>
+    )
+}
