d0930edc4b7118195f5c6eb51e3c661a997bf0e5
Gibson Cruz <gibson8fla@gmail.com>
2025-01-27 11:55:50 -0300

    chore: adds keyframes in the tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ export default {
     extend: {
       boxShadow: {
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
+      keyframes: {
+        'pulse': {
+          '0%, 100%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: .1
+          }
+        },
+      },
+      animation: {
+        'pulse': 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite'
       }
     },
   },
