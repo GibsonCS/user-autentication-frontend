270fdf595302fc2749470ac27a6fcf073915c947
GibsonCS <gibson.cruzds@gmail.com>
2025-02-12 14:19:12 -0300

    refactor: src/Components/ProtectedRoute/ProtectedRoute.tsx

--- src/Components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,31 +1,21 @@
 import { useEffect, useState } from "react";
-import { checkAuth } from "../../util/checkAuth";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
 import { Loading } from "../Loading/Loading";
+import { checkAuth } from "../../util/checkAuth";
 
 //receives and destructures an object with the requiredRoles property of the array string type
 export const ProtectedRoute = ({ requiredRoles }: { requiredRoles?: string[] }) => {
   const [hasRole, setHasRole] = useState<boolean | null>(null);
-  const navigate = useNavigate();
-  const { login } = useAuth();
 
   useEffect(() => {
-    const handleAuth = async () => {
+    const handleCheckAuth = async () => {
       const response = await checkAuth();
       const data = await response.json();
-      if (response.status === 200) {
-        login(data.username);
-      } else {
-        return navigate("/", { state: "Você não está autenticado" });
-      }
-      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-      requiredRoles?.some((role) => data.roles[0].includes(role))
-        ? setHasRole(true)
-        : setHasRole(false);
+      const existRole = requiredRoles?.includes(data.role);
+      existRole ? setHasRole(true) : setHasRole(false);
     };
-    handleAuth();
-  }, [login, navigate, requiredRoles]);
+    handleCheckAuth();
+  }, []);
 
   if (hasRole === null) {
     return <Loading />;
