19f0285219da2226f6934e5648682b3576990125
GibsonCS <gibson.cruzds@gmail.com>
2025-02-11 21:51:52 -0300

    chore: change function name src/util/login.ts

--- src/util/login.ts
+++ src/util/login.ts
@@ -1,12 +1,12 @@
 import { User } from "../interfaces/User";
 
-export const doLogin = async (username: string, password: string) => {
+export const auth = async (username: string, password: string) => {
   const credentials: User = {
     username: username,
     password: password,
   };
 
-  const response = await fetch(`${import.meta.env.VITE_URL_API}/auth/login`, {
+  const response = await fetch(`${import.meta.env.VITE_URL_API}/users/login`, {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify(credentials),
