523199dbc9159e6a8c0d40f4c0dddf9b39a6f49d
GibsonCS <gibson.cruzds@gmail.com>
2025-02-17 22:40:17 -0300

    refactor: src/util/login.ts

--- src/util/login.ts
+++ src/util/login.ts
@@ -6,14 +6,10 @@ export const auth = async (username: string, password: string) => {
     password: password,
   };
 
-  const response = await fetch(`${import.meta.env.VITE_URL_API}/users/login`, {
+  return await fetch(`${import.meta.env.VITE_URL_API}/users/login`, {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify(credentials),
     credentials: "include",
   });
-  const data = await response.json();
-  if (response.status === 200) {
-    return data;
-  }
 };
